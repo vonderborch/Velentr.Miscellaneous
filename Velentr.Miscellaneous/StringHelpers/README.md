466d3e9d9b69d0302732969e3f1ef88dd8ad2645
Christian Webber <ricky.webber@hotmail.com>
2022-02-04 18:23:28 -0500

    Added string splitting methods.

--- Velentr.Miscellaneous/StringHelpers/README.md
+++ Velentr.Miscellaneous/StringHelpers/README.md
@@ -5,3 +5,4 @@ Miscellaneous helper methods and things that are useful for various purposes.
 Class | Description | Min Supported Version | Example Usage
 ----- | ----------- | --------------------- | -------------
 StringSimilarity | Methods related to comparing the similarity of two strings. | 1.0.0 | `var similarity = StringSimilarity.GetDamerauLevenshteinDistance("string 1", "string 2");`
+StringSplitters | Methods related to splitting a string. | 1.0.3 | `var similarity = StringSplitters.SplitStringByChunkSize("AAABBBCCCDD", 3);`
---
+++ Velentr.Miscellaneous/StringHelpers/StringSplitters.cs
@@ -0,0 +1,63 @@
+﻿/// <file>
+/// Velentr.Miscellaneous\StringHelpers\StringSplitters.cs
+/// </file>
+///
+/// <copyright file="StringSplitters.cs" company="">
+/// Copyright (c) 2022 Christian Webber. All rights reserved.
+/// </copyright>
+///
+/// <summary>
+/// Implements the string splitters class.
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Velentr.Miscellaneous.StringHelpers
+{
+    /// <summary>
+    /// A string splitters.
+    /// </summary>
+    public static class StringSplitters
+    {
+        /// <summary>
+        /// The new line characters.
+        /// </summary>
+        private static string[] _newLineChars = new string[] { "\r\n", "\r", "\n" };
+
+        /// <summary>
+        /// Splits string by new lines.
+        /// </summary>
+        ///
+        /// <param name="str">  The string. </param>
+        ///
+        /// <returns>
+        /// A List&lt;string&gt;
+        /// </returns>
+        public static List<string> SplitStringByNewLines(string str)
+        {
+            return str.Split(_newLineChars, StringSplitOptions.None).ToList();
+        }
+
+        /// <summary>
+        /// Splits string by chunk size.
+        /// </summary>
+        ///
+        /// <param name="str">          The string. </param>
+        /// <param name="maxChunkSize"> The maximum size of the chunk. </param>
+        ///
+        /// <returns>
+        /// A List&lt;string&gt;
+        /// </returns>
+        public static List<string> SplitStringByChunkSize(string str, int maxChunkSize)
+        {
+            var output = new List<string>();
+            for (var i = 0; i < maxChunkSize; i += maxChunkSize)
+            {
+                output.Add(str.Substring(i, Math.Min(maxChunkSize, str.Length - i)));
+            }
+
+            return output;
+        }
+    }
+}
